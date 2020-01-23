92162ffa3e1cb3ab04464833bb19ba06178badfc
zzuberi <zzuberi@eng.ucsd.edu>
2020-01-22 22:31:35 -0800

    Fixed images, looks really weird

--- src/components/MLImage.jsx
+++ src/components/MLImage.jsx
@@ -5,65 +5,12 @@ import React, {useState} from 'react';
  * 
  */
 const Image = (props) => {
-    const [selected, setSelected] = useState(false);
 
-    // console.log("Displaying image");
+	const imageDir = ".";
 
-    // destructure image object received
-    const {
-        image_filename,
-        image_id,
-        image_timestamp,
-        image_date,
-        image_time,
-        image_file_size,
-        image_eccentricity,
-        image_orientation,
-        image_major_axis_length,
-        image_minor_axis_length,
-        image_height,
-        image_width,
-        image_solidity,
-        image_aspect_ratio,
-        image_estimated_volume,
-        image_area,
-        ml_model_name,
-        ml_user_labels,
-        ml_prediction,
-        ml_probability,
-        ml_prediction_timestamp,
-        annot_image_status,
-        annot_image_tags,
-        annot_machine_label,
-        annot_human_label
-    } = props.image;
-    const imageDir = "../../public";
-
-    // handle click event
-    const onImageClick = (e) => {
-
-        // call previous onClick
-        props.onClick(e, !selected);
-        
-        // change selected state
-        setSelected(!selected);
-
-    }
-
-    return(
+        return(
         <div className="Image">
-            {selected ? (
-            <div className="red-border">
-                <img src={imageDir + image_filename} alt={image_id} onClick={onImageClick}/>
-            </div>) : (
-                ml_user_labels == null ?
-                <img src={imageDir + image_filename} alt={image_id} onClick={onImageClick}/>
-                : (
-                    <div className="Annotated">
-                        <img src={imageDir + image_filename} alt={image_id} onClick={onImageClick}/>
-                    </div>
-                )
-            )}
+	    <img src={imageDir + props.image.images} alt={props.image.label}/>
         </div>
     );
 
